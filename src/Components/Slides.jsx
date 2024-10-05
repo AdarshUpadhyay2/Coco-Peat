bd127c5c9e0e4abcd663cf3f7afc75f10059b2dd
AdarshUpadhyay2 <tech@koreagro.com>
2024-10-05 15:12:16 +0530

    slide img path updated again

--- src/Components/Slides.jsx
+++ src/Components/Slides.jsx
@@ -2,224 +2,224 @@ const slides = {
   "Coco Substrates": [
     {
       imgSrc:
-        "img/Slides/slide1.jpg",
+        "../../img/Slides/slide1.jpg",
       title: "Coco Substrates",
     },
   ],
   "Coco Substrate Custom Mix": [
     {
       imgSrc:
-        "img/Slides/slide2.jpg",
+        "../../img/Slides/slide2.jpg",
       title: "Coco Substrate Custom Mix",
     },
   ],
   "Coco Substrate Fiber": [
     {
       imgSrc:
-        "img/Slides/slide3.jpg",
+        "../../img/Slides/slide3.jpg",
       title: "Coco Substrate Fiber",
     },
   ],
   "Coco Substrate Husk Chip Mix": [
     {
       imgSrc:
-        "img/Slides/slide4.jpeg",
+        "../../img/Slides/slide4.jpeg",
       title: "Coco Substrate Husk Chip Mix",
     },
   ],
   "Coco Substrate Fine/Coarse": [
     {
       imgSrc:
-        "img/Slides/slide5.jpg",
+        "../../img/Slides/slide5.jpg",
       title: "Coco Substrate Fine/Coarse",
     },
   ],
   "Coco Substrates Buffered": [
     {
       imgSrc:
-      "img/Slides/slide6.jpg",
+      "../../img/Slides/slide6.jpg",
       title: "",
     },
   ],
   "Coir Fiber": [
     {
       imgSrc:
-      "img/Slides/slide7.jpg",
+      "../../img/Slides/slide7.jpg",
       title: "Coir Fiber",
     },
   ],
   "Coir Fiber Bales": [
     {
       imgSrc:
-      "img/Slides/slide8.jpg",
+      "../../img/Slides/slide8.jpg",
       title: "Coir Fiber Bales",
     },
   ],
   "Coir Fiber Twisted Spool": [
     {
       imgSrc:
-        "img/Slides/slide9.jpg",
+        "../../img/Slides/slide9.jpg",
       title: "Coir Fiber Twisted Spool",
     },
   ],
   "Erosion Control": [
     {
       imgSrc:
-        "img/Slides/slide10.jpg",
+        "../../img/Slides/slide10.jpg",
       title: "Erosion Control",
     },
   ],
   "Coir Logs": [
     {
       imgSrc:
-        "img/Slides/slide11.jpg",
+        "../../img/Slides/slide11.jpg",
       title: "Coir Logs",
     },
   ],
   "Geo Textiles": [
     {
       imgSrc:
-        "img/Slides/slide12.jpg",
+        "../../img/Slides/slide12.jpg",
       title: "Geo Textiles",
     },
   ],
   "Garden Articles": [
     {
       imgSrc:
-        "img/Slides/slide13.jpg",
+        "../../img/Slides/slide13.jpg",
       title: "Garden Articles",
     },
   ],
   "Coco Basket": [
     {
       imgSrc:
-        "img/Slides/slide14.jpg",
+        "../../img/Slides/slide14.jpg",
       title: "Coco Basket",
     },
   ],
   "Coco Bricks": [
     {
       imgSrc:
-        "img/Slides/slide15.jpg",
+        "../../img/Slides/slide15.jpg",
       title: "Coco Bricks",
     },
   ],
   "Coco Pot": [
     {
       imgSrc:
-        "img/Slides/slide16.jpg",
+        "../../img/Slides/slide16.jpg",
       title: "Coco Pot",
     },
   ],
   "Coco Tabs or Disk": [
     {
       imgSrc:
-        "img/Slides/slide17.jpg",
+        "../../img/Slides/slide17.jpg",
       title: "Coco Disks",
     },
   ],
   "Growkit": [
     {
       imgSrc:
-        "img/Slides/slide18.jpg",
+        "../../img/Slides/slide18.jpg",
       title: "Growkit",
     },
   ],
   "Growbags": [
     {
       imgSrc:
-        "img/Slides/slide19.jpg",
+        "../../img/Slides/slide19.jpg",
       title: "Growbags",
     },
   ],
   "Growbags Bar Type": [
     {
       imgSrc:
-        "img/Slides/slide20.jpg",
+        "../../img/Slides/slide20.jpg",
       title: "Growbags Bar Type",
     },
   ],
   "Open Top Growbags": [
     {
       imgSrc:
-        "img/Slides/slide21.jpg",
+        "../../img/Slides/slide21.jpg",
       title: "Open Top Growbags",
     },
   ],
   "Plant Protection": [
     {
       imgSrc:
-        "img/Slides/slide22.jpg",
+        "../../img/Slides/slide22.jpg",
       title: "Plant Protection",
     },
   ],
   "Neem Oil": [
     {
       imgSrc:
-        "img/Slides/slide23.jpg",
+        "../../img/Slides/slide23.jpg",
       title: "Neem Oil",
     },
   ],
   "Neem Seed Powder": [
     {
       imgSrc:
-        "img/Slides/slide24.jpg",
+        "../../img/Slides/slide24.jpg",
       title: "Neem Seed Powder",
     },
   ],
   "Substrate Manufacturing": [
     {
       imgSrc:
-        "img/Slides/slide1.jpg",
+        "../../img/Slides/slide1.jpg",
       title: "Substrate Manufacturing",
     },
   ],
   "Greenhouse Cultivation": [
     {
       imgSrc:
-        "img/Slides/slide19.jpg",
+        "../../img/Slides/slide19.jpg",
       title: "Greenhouse Cultivation",
     },
   ],
   "Seed Germination": [
     {
       imgSrc:
-        "img/Slides/slide13.jpg",
+        "../../img/Slides/slide13.jpg",
       title: "Seed Gemination & Plant Propagation",
     },
   ],
   "Hobby Gardening": [
     {
       imgSrc:
-        "img/Slides/slide13.jpg",
+        "../../img/Slides/slide13.jpg",
       title: "Hobby Gardening",
     },
   ],
   "Soil Erosion Control": [
     {
       imgSrc:
-        "img/Slides/slide10.jpg",
+        "../../img/Slides/slide10.jpg",
       title: "Soil Erosion Control",
     },
   ], 
   "Upholstery and Insulation": [
     {
       imgSrc:
-        "img/Slides/slide7.jpg",
+        "../../img/Slides/slide7.jpg",
       title: "Upholstery and Insulation",
     },
   ],
   "Natural plant protection": [
     {
       imgSrc:
-        "img/Slides/slide22.jpg",
+        "../../img/Slides/slide22.jpg",
       title: "Natural plant protection",
     },
   ],
   "Coco Disks": [
     {
       imgSrc:
-        "img/Slides/slide25.jpg",
+        "../../img/Slides/slide25.jpg",
       title: "",
     },
   ],
